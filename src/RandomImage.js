e5ff74795d39c7aec25e39856c68b183264892f0
Snehashish7 <snehashishghosh21@gmail.com>
2023-07-09 12:13:27 +0530

    finished changes

--- src/RandomImage.js
+++ src/RandomImage.js
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react';
 
 const RandomImage = () => {
-    const [randomImage, setRandomImage] = useState('');
+  const [randomImage, setRandomImage] = useState('');
 
-    useEffect(() => {
-        const fetchRandomImage = async () => {
-            try {
-                const response = await fetch('https://source.unsplash.com/random');
-                setRandomImage(response.url);
-            } catch (error) {
-                console.error('Error fetching random image:', error);
-            }
-        };
+  useEffect(() => {
+    const fetchRandomImage = async () => {
+      try {
+        const response = await fetch('https://source.unsplash.com/random');
+        setRandomImage(response.url);
+      } catch (error) {
+        console.error('Error fetching random image:', error);
+      }
+    };
 
-        fetchRandomImage();
-    }, []);
+    fetchRandomImage();
+  }, []);
 
-    return (
-        <div className="random-image">
-            {randomImage && <img src={randomImage} alt="Random" />}
-        </div>
-    );
+  return (
+    <div className="random-image">
+      {randomImage && <img src={randomImage} alt="Random" />}
+    </div>
+  );
 };
 
 export default RandomImage;
